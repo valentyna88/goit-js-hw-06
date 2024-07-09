9bb1853ac32ac2c3a3c4183d499742625a3578a1
Valentyna Taranchuk <valyataranchuk8@gmail.com>
2024-07-09 17:01:56 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,18 @@
 class Storage {
   #items;
+
   constructor(items) {
     this.#items = items;
   }
+
   getItems() {
     return this.#items;
   }
+
   addItem(newItem) {
     this.#items.push(newItem);
   }
+
   removeItem(itemToRemove) {
     const index = this.#items.indexOf(itemToRemove);
     if (index !== -1) {
@@ -16,6 +20,7 @@ class Storage {
     }
   }
 }
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
